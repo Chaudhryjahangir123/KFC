8b3e5aebfdb942d885190505fb448de83d4d300f
moin1360 <139452235+moin1360@users.noreply.github.com>
2023-09-05 23:06:27 +0500

    slider1

--- moin.js
+++ moin.js
@@ -1,34 +1,71 @@
 // Slider1
-const sContainer = document.querySelector('.s');
-const sSlides = document.querySelectorAll('.s2');
-const numSlides = sSlides.length;
-let currentIndex = 0;
 
-function showSlide(index) {
-    if (index >= 0 && index < numSlides) {
-        sContainer.scrollLeft = sSlides[index].offsetLeft;
-    }
+const slides = document.querySelectorAll(".s2");
+var counter = 0;
+var maxClicks = 4; // Set the maximum number of clicks
+
+slides.forEach((s2, index) => {
+  s2.style.right = `${index * 100}%`;
+});
+
+const goPrev = () => {
+  if (counter > 0) {
+    counter--;
+    slideImage();
+  }
+  updateButtonState();
+};
+
+const goNext = () => {
+  if (counter < slides.length - 1) {
+    counter++;
+    slideImage();
+  }
+  updateButtonState();
+};
+
+const slideImage = () => {
+  slides.forEach((slide) => {
+    slide.style.transform = `translateX(-${counter * 100}%)`;
+  });
+};
+
+const prevButton = document.getElementById("prevButton"); // Add id="prevButton" to your previous button element
+const nextButton = document.getElementById("nextButton"); // Add id="nextButton" to your next button element
+
+function updateButtonState() {
+  if (counter === 0) {
+    prevButton.disabled = true; // Disable the previous button when at the beginning
+  } else {
+    prevButton.disabled = false;
+  }
+
+  if (counter === slides.length - 1) {
+    nextButton.disabled = true; // Disable the next button when at the end
+  } else {
+    nextButton.disabled = false;
+  }
+
+  // Check if the maximum number of clicks has been reached, and disable the buttons if necessary
+  if (counter === 0 && maxClicks <= 0) {
+    prevButton.disabled = true;
+    nextButton.disabled = true;
+  }
+
+  if (counter === slides.length - 1 && maxClicks <= 0) {
+    prevButton.disabled = true;
+    nextButton.disabled = true;
+  }
+
+  // Decrement the maxClicks counter
+  maxClicks--;
 }
 
-function nextSlide() {
-    currentIndex = (currentIndex + 1) % numSlides;
-    showSlide(currentIndex);
-}
+// Initial slide
+slideImage();
+updateButtonState();
 
-function prevSlide() {
-    currentIndex = (currentIndex - 1 + numSlides) % numSlides;
-    showSlide(currentIndex);
-}
 
-// Initially, show the first slide
-showSlide(currentIndex);
-
-// Add event listeners to the left and right navigation buttons
-const left1Button = document.getElementById('left1');
-const right1Button = document.getElementById('right1');
-
-left1Button.addEventListener('click', prevSlide);
-right1Button.addEventListener('click', nextSlide);
 // End of slider1
 
 
@@ -46,9 +83,13 @@ const firstCardWidth = slider.querySelector(".slider01").offsetWidth;
 const leftButton = slideContainer.querySelector("#left"); // Left button
 const rightButton = slideContainer.querySelector("#right"); // Right button
 
+
+
 let isDragging = false;
 let startX;
 let startScrollLeft;
+
+
 
 // Add event listener for the left button to scroll the carousel to the left
 leftButton.addEventListener("click", () => {
@@ -59,7 +100,6 @@ leftButton.addEventListener("click", () => {
 rightButton.addEventListener("click", () => {
   slider.scrollLeft += firstCardWidth; // Scroll right
 });
-
 const dragStart = (e) => {
   isDragging = true;
   slider.classList.add("dragging");
@@ -67,22 +107,20 @@ const dragStart = (e) => {
   startX = e.pageX;
   startScrollLeft = slider.scrollLeft;
 };
-
 const dragging = (e) => {
   if (!isDragging) return;
   // Update the scroll position of the carousel based on the cursor movement
   slider.scrollLeft = startScrollLeft - (e.pageX - startX);
 };
-
 const dragStop = () => {
   isDragging = false;
   slider.classList.remove("dragging");
 };
 
+
+
+
 slider.addEventListener("mousedown", dragStart);
 slider.addEventListener("mousemove", dragging);
 document.addEventListener("mouseup", dragStop);
-
-
 // Slider 2 END 
-
